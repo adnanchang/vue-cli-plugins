873fd512639143334218e79aad33c1bf65288b79
Muhammad Adnan <m.adnanchangezi@gmail.com>
2022-10-26 17:27:00 +0200

    Remove fonts

--- packages/vue-cli-plugin-vuetify/generator/tools/fonts.js
+++ packages/vue-cli-plugin-vuetify/generator/tools/fonts.js
@@ -1,21 +1,5 @@
 const helpers = require('./helpers')
 const fonts = {
-  mdi: {
-    package: {
-      '@mdi/font': '5.9.55',
-    },
-    import: '@mdi/font/css/materialdesignicons.css',
-    link:
-      '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">',
-  },
-  md: {
-    package: {
-      'material-design-icons-iconfont': '6.1.0',
-    },
-    import: 'material-design-icons-iconfont/dist/material-design-icons.css',
-    link:
-      '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons">',
-  },
   fa: {
     package: {
       '@fortawesome/fontawesome-free': '5.15.3',
@@ -32,36 +16,21 @@ const fonts = {
     link:
       '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">',
   },
-  roboto: {
-    package: {
-      'roboto-fontface': '*',
-    },
-    import: 'roboto-fontface/css/roboto/roboto-fontface.css',
-    link:
-      '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">',
-  },
 }
 
 function addDependencies (api, iconFont) {
   api.extendPackage({
     dependencies: {
-      ...fonts.roboto.package,
-      ...fonts[iconFont].package,
     },
   })
 }
 
 function addImports (api, iconFont) {
-  api.injectImports(api.entryFile, `import '${fonts.roboto.import}'`)
-  api.injectImports(api.entryFile, `import '${fonts[iconFont].import}'`)
 }
 
 function addLinks (api, iconFont) {
   helpers.updateFile(api, './public/index.html', lines => {
     const lastLink = lines.reverse().findIndex(line => line.match(/^\s*<\/head>/))
-
-    lines.splice(lastLink + 1, 0, `    ${fonts.roboto.link}`)
-    lines.splice(lastLink + 1, 0, `    ${fonts[iconFont].link}`)
 
     return lines.reverse()
   })
